ea29c7c2a1b872e257fc73d0face8f7593e2350d
sainiharshit322 <146534460+sainiharshit322@users.noreply.github.com>
2024-08-02 19:44:33 +0530

    Delete employee/employee directory

--- employee/employee/src/main/java/com/harshit/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.harshit.repository;
-
-import com.harshit.entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-}
